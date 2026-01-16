a04cb61ac9a00dbfd4c207a6c2bf658367cf34af
wyzen-re <monkeydzenso74@gmail.com>
2026-01-16 17:01:26 +0100

    Adjust loader removal timeout from 400ms to 450ms

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 window.addEventListener("load", () => {
   const loader = document.getElementById("loader");
   loader.style.opacity = "0";
-  setTimeout(() => loader.remove(), 400);
+  setTimeout(() => loader.remove(), 450);
 });
 
 /* ===== SCROLL REVEAL ===== */
